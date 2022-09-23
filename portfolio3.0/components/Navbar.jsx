ce20b49cfab728e46321b3cf2516088c35daabb3
BobTarino <bob.tarino@gmail.com>
2022-09-22 21:03:23 -0400

    stylized navbar with hover effect and mapped out array of menu items

--- portfolio3.0/components/Navbar.jsx
+++ portfolio3.0/components/Navbar.jsx
@@ -2,7 +2,31 @@ import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
-  return (
+    
+    const links = [
+        {
+            id:1,
+            link: 'home'
+        },
+        {
+            id:2,
+            link: 'portfolio'
+        },
+        {
+            id:3,
+            link: 'experience'
+        },
+        {
+            id:4,
+            link: 'me'
+        },
+        {
+            id:5,
+            link: 'contact'
+        },
+    ]
+  
+    return (
     <div className='w-full h20 z-10 fixed bg-white text-black duration-300 ease-in'>
         <div className='flex justify-between items-center w-full h-full max-w-screen-xl mx-auto p-4'>
             {/* cliickable name link */}
@@ -12,13 +36,13 @@ const Navbar = () => {
             
             {/* list items */}
             <div>
-                <ul>
-                    <Link href='/#home'>
-                        <li className='ml-10 textsm uppercase cursor-pointer duration-200 ease-out hover:scale-105 tracking-wider'>home</li>
-                        {/* youtube vid at 25:37 */}
-                    </Link>
+                <ul className='hidden md:flex'>
+                    {links.map(({id, link}) => (
+                        <Link key={id} href={`/#{link}`}>
+                            <li className='ml-10 textsm uppercase cursor-pointer duration-200 ease-out hover:scale-105 tracking-wider'>{link}</li>
+                        </Link>
+                    ))}
                 </ul>
-
             </div>
         </div>
     </div>
